da08ebcad27d841b31a3925c04dcd04d71a825dd
Weiye <60509897+wl44-li@users.noreply.github.com>
2020-07-08 15:36:09 +0100

    improved data filter & graph detail

--- simpleGraph.py
+++ simpleGraph.py
@@ -17,15 +17,14 @@ def graph(data, filepath):
     fig.savefig(filename + 'sec_fig.png', dpi = 120)
 
     # Plot data every 60 seconds (minute interval)
-    df = data[data.index % 60 == 0]
-    df = df.reset_index(drop = True)
-
+    df = data.groupby(np.arange(len(data))//60).mean()
     ax = df.plot()
     fig = ax.get_figure()
     ax.set_xlabel("Time(min)")
     ax.set_ylabel("Growth(dB)")
     fig.set_size_inches(16, 9)
     fig.savefig(filename + 'min_fig.png', dpi = 120)
+    
     # show on console (DEBUG purpose)
     plt.show()
 
@@ -38,10 +37,10 @@ def thresholdGraph(data, threshold, title, filepath):
 
     else:
         filename = os.path.splitext(filepath)[0]
-        # Plot data every 60 seconds
-        df = data[data.index % 60 == 0]
-        df = df.reset_index(drop = True)
-
+        
+        # Plot data every 60 seconds (rolling average)
+        df = data.groupby(np.arange(len(data))//60).mean()
+        df.to_csv("clean.csv", index = False)
         ax = df.plot()
         ax_ctrl = df.reset_index().plot(x = 'index', y = 1, kind = 'scatter', ax = ax, c = "b")
 
@@ -54,10 +53,10 @@ def thresholdGraph(data, threshold, title, filepath):
             colors = np.where(df.iloc[:, (i-1)] > df.iloc[:, 0]*threshold, 'r', 'g')
             ax_i = df.reset_index().plot(x = 'index', y = i, kind = 'scatter', ax = ax, c = colors, marker = "|")
 
-            # Consecutive shift of 10 minutes - changeble via GUI?
-            if (len(green_list) > 10):
-                for i in range(0, len(green_list) - 10):
-                    if (green_list[i] + 10 - 1 == green_list[i + 10 - 1]):
+            # Consecutive shift of 2 minute (120 seconds)
+            if (len(green_list) > 5):
+                for i in range(0, len(green_list) - 2):
+                    if (green_list[i] + 2 == green_list[i + 2]):
                         time_tick.append(green_list[i])
                         break
             else:
@@ -77,11 +76,18 @@ def thresholdGraph(data, threshold, title, filepath):
                     txt = txt + 'Channel ' + str(i + 1) + ': '+  df.columns[i+1] + ' at ' + str(time_tick[i]) + " minute"
             else:
                 txt = txt + 'Channel ' + str(i + 1) + ': '+  df.columns[i+1] + ' ' + time_tick[i] + ' \n'
-
+        
         fig = ax.get_figure()
         fig.text(0.45, -0.05, txt, ha = 'left',  bbox = dict(facecolor = 'red', alpha = 0.4))
         fig.set_size_inches(16, 9, forward = True)
         fig.savefig(filename + '_' + (str)(threshold * 100) + '%_threshold.png', dpi = 120, bbox_inches = "tight")
+
+        # Zoom in graph to 60 mins mark
+        ax_zoom = ax
+        ax_zoom.set_xlim([0, 60])
+        ax_zoom.set_ylim([-1, 2.5])
+        fig = ax_zoom.get_figure()
+        fig.savefig(filename + '_' + (str)(threshold * 100) + '%_threshold_60min.png', dpi = 120, bbox_inches = "tight")
         plt.show()
 
 
@@ -92,13 +98,11 @@ def threshold_errorbar(data, error, threshold, title, filepath):
         return None
     else:
         filename = os.path.splitext(filepath)[0]
-        df = data[data.index % 60 == 0]
-        df = df.reset_index(drop = True)
+        df = data.groupby(np.arange(len(data))//60).mean()
 
         df_err = error[error.index % 60 == 0]
         df_err = df_err.reset_index(drop = True)
-
-        ax = df.plot(yerr = df_err, linestyle = '-', linewidth = 0.6)
+        ax = df.plot(yerr = df_err, linestyle = '-', linewidth = 0.8)
         ax_ctrl = df.reset_index().plot(x = 'index', y = 1, kind = 'scatter', ax = ax, c = "b")
         time_tick = []
 
@@ -108,11 +112,11 @@ def threshold_errorbar(data, error, threshold, title, filepath):
 
             # above threashold is RED, below is GREEN
             colors = np.where(df.iloc[:, (i-1)] > df.iloc[:, 0]*threshold, 'r', 'g')
-            ax_i = df.reset_index().plot(x = 'index', y = i, kind = 'scatter', ax = ax, c = colors, marker = "x")
+            ax_i = df.reset_index().plot(x = 'index', y = i, kind = 'scatter', ax = ax, c = colors, marker = ".")
 
-            if (len(green_list) > 10):
-                for i in range(0, len(green_list) - 10):
-                    if (green_list[i] + 10 - 1 == green_list[i + 10 - 1]):
+            if (len(green_list) > 5):
+                for i in range(0, len(green_list) - 2):
+                    if (green_list[i] + 2 == green_list[i + 2]):
                         time_tick.append(green_list[i])
                         break
             else:
@@ -131,10 +135,18 @@ def threshold_errorbar(data, error, threshold, title, filepath):
                 else:
                     txt = txt + 'Channel ' + str(i + 1) + ': '+  df.columns[i + 1] + ' at ' + str(time_tick[i]) + " minute"
             else:
-                txt = txt + 'Channel ' + str(i + 1) + ': '+  df.columns[i+1] + ' ' + time_tick[i] + ' \n'
+                txt = txt + 'Channel ' + str(i + 1) + ': '+  df.columns[i + 1] + ' ' + time_tick[i] + ' \n'
 
         fig = ax.get_figure()
         fig.text(0.45, -0.05, txt, ha = 'left', bbox = dict(facecolor = 'red', alpha = 0.4))
         fig.set_size_inches(16, 9, forward = True)
         fig.savefig(filename + '_' + (str)(threshold * 100) + '%_threshold.png', dpi = 120, bbox_inches = "tight")
+                
+        # Zoom in graph to 60 mins mark
+        ax_zoom = ax
+        ax_zoom.set_xlim([0, 60])
+        ax_zoom.set_ylim([-1, 2.5])
+        fig = ax_zoom.get_figure()
+        fig.savefig(filename + '_' + (str)(threshold * 100) + '%_threshold_60min.png', dpi = 120, bbox_inches = "tight")
         plt.show()
+
